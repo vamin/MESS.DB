3447a5760fbfc9d3047822c749b578bda19a996a
Victor Amin <victor.amin@gmail.com>
2013-10-15 13:19:18 -0500

    updated qsub scripts

--- methods/balloon141/qsub.sh
+++ methods/balloon141/qsub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~
-echo "module load python; module load openbabel; python2.7 /projects/victor/messdb/mess select -s $1 'select inchikey from molecule;' | python2.7 /projects/victor/messdb/mess calculate -m mmff94s_gen3d_ob232" | qsub -V -l walltime=4:00:00 -l nodes=1:ppn=1 -N 'mmff94s_gen3d_ob232'
+echo "module load python; module load openbabel; module load balloon; module load mess; mess select -s $1 'select inchikey from molecule;' | mess calculate -m balloon141" | qsub -V -l walltime=2:00:00:00 -l nodes=1:ppn=1 -N 'balloon141'
--- methods/pm7_mopac2012/qsub.sh
+++ methods/pm7_mopac2012/qsub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~
-echo "module load openbabel; python /projects/victor/messdb/mess select -s $1 'select inchikey from molecule;' | python /projects/victor/messdb/mess calculate -m pm7_mopac2012" | qsub -V -l nodes=1:ppn=1 -N 'pm7_mopac2012'
+echo "module load python; module load openbabel; module load mopac; module load mess; mess select -s $1 'select inchikey from molecule;' | mess calculate -m pm7_mopac2012" | qsub -V -l nodes=1:ppn=1 -N 'pm7_mopac2012'
