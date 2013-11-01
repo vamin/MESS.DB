7d0275657a17dfc73f0dcbe11446b0f84cd60246
Victor Amin <victor.amin@gmail.com>
2013-10-31 23:09:05 -0500

    balloon method no longer attempts to strip salts, uses mmff94s

--- methods/balloon141/method.py
+++ methods/balloon141/method.py
@@ -27,12 +27,14 @@ class Method(AbstractMethod):
 
     def check_dependencies(self):
         try:
-            balloon = subprocess.Popen(['balloon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            balloon = subprocess.Popen(['balloon'], stdout=subprocess.PIPE, 
+                                       stderr=subprocess.PIPE)
             balloon_version = balloon.stdout.read().split()[2]
             if (LooseVersion(balloon_version) < LooseVersion('1.4.1')):
                 sys.exit("Balloon version must be >=1.4.1.")
         except OSError:
-            sys.exit("The " + self.method_name + " method requires balloon (" + self.prog_url + ").")
+            sys.exit('The ' + self.method_name + ' method requires balloon (' + 
+                     self.prog_url + ').')
         return True
     
     def execute(self, args):
@@ -42,27 +44,37 @@ class Method(AbstractMethod):
         out_dir = os.path.realpath(os.path.join(inchikey_dir, method_dir))
         self.setup_dir(out_dir)
         xyz_out = os.path.join(out_dir, inchikey + '.xyz')
-        sdf_out = os.path.realpath(os.path.join(inchikey_dir, method_dir, inchikey + '.sdf'))
+        sdf_out = os.path.realpath(os.path.join(inchikey_dir, method_dir, 
+                                                inchikey + '.sdf'))
         balloon_stdout = ''
         balloon_stderr = ''
         if not self.check(xyz_out):
-            smiles = self.c.execute('SELECT smiles FROM molecule WHERE inchikey=?', (inchikey,)).next()
-            seed = binascii.crc32(inchikey) & 0xffffffff # returns positive 32-bit integer
+            q = 'SELECT smiles FROM molecule WHERE inchikey=?'
+            smiles = self.c.execute(q, (inchikey,)).next()
+            # get positive 32-bit integer
+            seed = binascii.crc32(inchikey) & 0xffffffff
             try:
                 os.remove(sdf_out)
             except OSError:
                 pass
-            balloon = subprocess.Popen(['/home/victor/software/balloon_x86_64/balloon', '-v', '1', '--maxtime', '256', '--randomSeed', str(seed), '--nGenerations', '1024', '--singleconf', '--fullforce', '--stripSalts', smiles['smiles'], sdf_out], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            balloon = subprocess.Popen(['balloon', '-v', '1', '--maxtime', 
+                                        '1024', '--randomSeed', str(seed), 
+                                        '--nGenerations', '1024', 
+                                        '--singleconf', '--fullforce', 
+                                        smiles['smiles'], sdf_out], 
+                                       stdout=subprocess.PIPE, 
+                                       stderr=subprocess.PIPE)
             balloon_stdout = balloon.stdout.read()
             balloon_stderr = balloon.stderr.read()
             try:
                 mol = pybel.readfile('sdf', str(sdf_out)).next()
             except IOError:
-                sdf_bad = os.path.join(os.path.dirname(__file__), '../../', inchikey + '_bad.sdf')
+                sdf_bad = os.path.join(os.path.dirname(__file__), 
+                                       '../../', inchikey + '_bad.sdf')
                 sdf_out = os.path.join(out_dir, inchikey + '_bad.sdf')
                 os.rename(sdf_bad, sdf_out)
                 mol = pybel.readfile('sdf', str(sdf_out)).next()
-            mol.localopt(forcefield='uff', steps=128)
+            mol.localopt(forcefield='mmff94s', steps=128)
             mol.write('xyz', str(xyz_out))
             self.check(xyz_out)
         else:
@@ -90,20 +102,22 @@ class Method(AbstractMethod):
     
     def log(self, args, inchikey_dir, messages):
         base_log_path = os.path.join(inchikey_dir, args['inchikey'] + '.log')
-        method_log_path = os.path.join(inchikey_dir, args['method_dir'], args['inchikey'] + '.log')
-        self.add_messages_to_log(base_log_path, self.method_name, ['status: ' + self.status])
+        method_log_path = os.path.join(inchikey_dir, args['method_dir'], 
+                                       args['inchikey'] + '.log')
+        self.add_messages_to_log(base_log_path, self.method_name, 
+                                 ['status: ' + self.status])
         if (messages):
-            self.add_messages_to_log(method_log_path, self.method_name, messages)
+            self.add_messages_to_log(method_log_path, self.method_name, 
+                                     messages)
     
     def setup_parameters(self):
         self.insert_method_parameter('-v', '1')
-        self.insert_method_parameter('--maxtime', '256')
+        self.insert_method_parameter('--maxtime', '1024')
         self.insert_method_parameter('--randomSeed', 'crc32(inchikey)')
         self.insert_method_parameter('--nGenerations', '1024')
         self.insert_method_parameter('--singleconf', '')
-        self.insert_method_parameter('--stripSalts', '')
-        self.insert_method_parameter('-n', '')
-        self.insert_method_parameter("pybel mol.localopt(forcefield='uff', steps=128)", '')
+        self.insert_method_parameter("pybel mol.localopt(forcefield='uff', \
+                                     steps=128)", '')
         
     def import_properties(self):
         pass
