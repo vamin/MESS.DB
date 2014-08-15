aef4082b4eab0c1cc5f821d76e897153be64e21a
Victor Amin <victor.amin@gmail.com>
2014-08-14 20:15:45 -0500

    add parsing for localized orbitals

--- mess/methods/mopac.py
+++ mess/methods/mopac.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import codecs
-import math
+import json
 import os
 import subprocess
 import sys
@@ -10,6 +10,7 @@ import time
 
 from _method import AbstractMethod
 from utils import get_inchikey_dir, setup_dir, write_to_log
+
 
 class Mopac(AbstractMethod):
     # method info
@@ -44,11 +45,11 @@ class Mopac(AbstractMethod):
         
         """
         try:
-            subprocess.check_output(['MOPAC2012.exe'], 
+            subprocess.check_output(['MOPAC2012.exe'],
                                     stderr=open(os.devnull, 'w'))
         except OSError:
             sys.exit(('The %s method requires MOPAC2012.exe (%s) to be '
-                      'installed and in PATH.') % (self.method_name, 
+                      'installed and in PATH.') % (self.method_name,
                                                    self.prog_url))
         return True
     
@@ -65,10 +66,10 @@ class Mopac(AbstractMethod):
         setup_dir(out_dir)
         mop_file = os.path.join(out_dir, '%s.mop' % inchikey)
         out_file = os.path.join(out_dir, '%s.out' % inchikey)
-        xyz_in = os.path.abspath(os.path.join(inchikey_dir, parent_method_dir, 
-                                 '%s.xyz' % inchikey))
-        xyz_out = os.path.abspath(os.path.join(out_dir, 
-                                  '%s.xyz' % inchikey))
+        xyz_in = os.path.abspath(os.path.join(inchikey_dir, parent_method_dir,
+                                              '%s.xyz' % inchikey))
+        xyz_out = os.path.abspath(os.path.join(out_dir,
+                                               '%s.xyz' % inchikey))
         if not self.check(out_file, xyz_out):
             keywords = ''
             for k, v in self.parameters.items():
@@ -81,31 +82,31 @@ class Mopac(AbstractMethod):
                      'FROM molecule_method_property mpp '
                      'JOIN property p ON mpp.property_id = p.property_id '
                      "WHERE p.name='charge' AND mpp.inchikey=?")
-            r = self.db.execute(query, (inchikey,)).fetchone()
-            keywords += 'CHARGE=%i' % r.charge
-            babel = subprocess.Popen(['obabel', '-ixyz', xyz_in, '-omop', 
-                                      '-xk' + keywords], 
-                                     stdout=codecs.open(mop_file, 'w', 
-                                                        'utf-8'), 
+            charge = self.db.execute(query, (inchikey,)).fetchone()[0]
+            keywords += 'CHARGE=%i' % charge
+            babel = subprocess.Popen(['obabel', '-ixyz', xyz_in, '-omop',
+                                      '-xk' + keywords],
+                                     stdout=codecs.open(mop_file, 'w',
+                                                        'utf-8'),
                                      stderr=subprocess.PIPE)
             babel_stderr = babel.stderr.read()
             pwd = os.getcwd()
-            os.chdir(out_dir) # mopac unhappy if not run in same dir as input
+            os.chdir(out_dir)  # mopac unhappy if not run in same dir as input
             subprocess.Popen(['MOPAC2012.exe', '%s.mop' % inchikey]).wait()
             os.chdir(pwd)
             self.moo_to_xyz(os.path.abspath(out_file), xyz_out)
             if self.check(out_file, xyz_out):
                 yield self.get_timing_query(inchikey, path_id, start)
-                for query, values in self.import_properties(inchikey, 
-                                                            path_id, 
+                for query, values in self.import_properties(inchikey,
+                                                            path_id,
                                                             out_file):
                     yield query, values
             else:
                 print(babel_stderr, file=sys.stderr)
         else:
             self.status = 'calculation skipped'
-            for query, values in self.import_properties(inchikey, 
-                                                        path_id, 
+            for query, values in self.import_properties(inchikey,
+                                                        path_id,
                                                         out_file):
                 yield query, values
     
@@ -125,13 +126,13 @@ class Mopac(AbstractMethod):
         """
         xyz_check = False
         moo_check = False
-        try: # xyz check
-            with codecs.open(xyz_out, encoding='utf-8') as f:
-                for i, l in enumerate(f):
-                    if (i == 0):
-                        atoms = l.strip()
+        try:  # xyz check
+            with codecs.open(xyz_out, encoding='utf-8') as file_:
+                for i, line in enumerate(file_):
+                    if i == 0:
+                        atoms = line.strip()
             try:
-                if (int(atoms) == i - 1):
+                if int(atoms) == i - 1:
                     xyz_check = True
             except (UnboundLocalError, ValueError):
                 # 0 lines in file, i not set
@@ -141,14 +142,14 @@ class Mopac(AbstractMethod):
             pass
         # moo check
         try:
-            tail = subprocess.Popen(['tail', '-n 2', moo_out], 
-                                    stdout=subprocess.PIPE, 
+            tail = subprocess.Popen(['tail', '-n 2', moo_out],
+                                    stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
-            if (tail.stdout.read().strip() == '== MOPAC DONE =='):
+            if tail.stdout.read().strip() == '== MOPAC DONE ==':
                 moo_check = True
         except:
             pass
-        if (moo_check and xyz_check):
+        if moo_check and xyz_check:
             self.status = 'PM7 calculation completed successfully'
             return True
         else:
@@ -164,7 +165,7 @@ class Mopac(AbstractMethod):
         
         """
         base_log_path = os.path.join(inchikey_dir, '%s.log' % inchikey)
-        method_log_path = os.path.join(inchikey_dir, method_dir, 
+        method_log_path = os.path.join(inchikey_dir, method_dir,
                                        '%s.log' % inchikey)
         write_to_log(base_log_path, ['status: %s' % self.status])
         write_to_log(method_log_path, ['status: %s' % self.status])
@@ -178,31 +179,33 @@ class Mopac(AbstractMethod):
             moo_out: Path to a mopac output file.
         
         """
-        try: # moo check
-            with codecs.open(moo_out, encoding='utf-8') as f:
+        try:  # moo check
+            with codecs.open(moo_out, encoding='utf-8') as file_:
                 # parse out properties
-                for line in f:
-                    if ('FINAL HEAT OF FORMATION' in line):
+                localisation = 'not started'
+                localized_orbitals = []
+                for line in file_:
+                    if 'FINAL HEAT OF FORMATION' in line:
                         s = line.split()
                         heat_of_formation_kcal = s[5]
                         heat_of_formation_kJ = s[8]
-                    if ('TOTAL ENERGY' in line):
+                    if 'TOTAL ENERGY' in line:
                         total_energy = line.split()[3]
-                    if ('ELECTRONIC ENERGY' in line):
+                    if 'ELECTRONIC ENERGY' in line:
                         s = line.split()
                         electronic_energy = s[3]
                         point_group = s[7]
-                    if ('CORE-CORE REPULSION' in line):
+                    if 'CORE-CORE REPULSION' in line:
                         core_repulsion = line.split()[3]
-                    if ('COSMO AREA' in line):
+                    if 'COSMO AREA' in line:
                         cosmo_area = line.split()[3]
-                    if ('COSMO VOLUME' in line):
+                    if 'COSMO VOLUME' in line:
                         cosmo_volume = line.split()[3]
-                    if ('GRADIENT NORM' in line):
+                    if 'GRADIENT NORM' in line:
                         gradient_norm = line.split()[3]
-                    if ('IONIZATION POTENTIAL' in line):
+                    if 'IONIZATION POTENTIAL' in line:
                         ionization_potential = line.split()[3]
-                    if ('HOMO LUMO ENERGIES' in line):
+                    if 'HOMO LUMO ENERGIES' in line:
                         s = line.split()
                         homo = s[5]
                         try:
@@ -212,13 +215,24 @@ class Mopac(AbstractMethod):
                             if homo.count('.') > 1:
                                 s = homo.split('.')
                                 homo = s[0] + '.' + s[1]
-                    if ('NO. OF FILLED LEVELS' in line):
+                    if 'NO. OF FILLED LEVELS' in line:
                         filled_levels = line.split()[5]
-                    if ('COMPUTATION TIME' in line):
-                        # no properties after, might as well skip ahead
-                        break
+                    if 'LOCALISATION VALUE' in line:
+                        localisation = 'started'
+                    if 'LOCALIZED ORBITALS' in line:
+                        localisation = 'ended'
+                    if localisation == 'started':
+                        s = line.split()
+                        try:
+                            float(s[0])
+                            composition = dict()
+                            for idx in xrange(2, len(s), 3):
+                                composition[int(s[idx])] = float(s[idx + 1])
+                            localized_orbitals.append(composition)
+                        except (IndexError, ValueError):
+                            continue
         except IOError:
-            sys.exit('Expected %s to be a valid path. Check method code.' % 
+            sys.exit('Expected %s to be a valid path. Check method code.' %
                      moo_out)
         # insert properties into db
         try:
@@ -313,8 +327,25 @@ class Mopac(AbstractMethod):
         except UnboundLocalError as e:
             print(e, file=sys.stderr)
             self.status = 'some properties not parsed'
+        try:
+            localized_orbitals_dict = {'HOMO': localized_orbitals[-1]}
+            n = 1
+            for composition in reversed(localized_orbitals[:-1]):
+                mo = 'HOMO-%i' % n
+                localized_orbitals_dict[mo] = composition
+                n += 1
+                if n == 5:
+                    break
+            yield self.get_insert_property_query(
+                inchikey, method_path_id,
+                'LOCALIZED ORBITALS', 'MOPAC property', 'JSON',
+                json.dumps(localized_orbitals_dict, sort_keys=True), '')
+        except (UnboundLocalError, IndexError) as e:
+            print(e, file=sys.stderr)
+            self.status = 'some properties not parsed'
 
-    def moo_to_xyz(self, moo, xyz):
+    @classmethod
+    def moo_to_xyz(cls, moo, xyz):
         """Convert Mopac output to xyz.
         
         Args:
@@ -333,10 +364,10 @@ class Mopac(AbstractMethod):
                 # parse xyz
                 s = line.split()
                 try:
-                    if int(s[0]) + 1 > 0: # check if first col is a number
+                    if int(s[0]) + 1 > 0:  # check if first col is a number
                         xyz_coords.append((s[1], s[2], s[4], s[6]))
                 except (IndexError, ValueError, TypeError):
-                    pass # not a coordinate line
+                    pass  # not a coordinate line
                 if 'Empirical Formula:' in line:
                     # check integrity
                     if not len(xyz_coords) == int(line.split()[-2]):
