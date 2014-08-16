bdd6db3eaa92c7879f2526320580365bb0d19d7e
Victor Amin <victor.amin@gmail.com>
2014-08-16 09:46:01 -0500

    suppress broken pipe errors, correct import

--- mess/__main__.py
+++ mess/__main__.py
@@ -2,7 +2,7 @@
 """MESS.DB main module
 
 This module runs MESS.DB. The primary responibility of this code is to load a
-user tool and execute it.
+mess tool and execute it.
 
 Victor Amin 2013-2014
 """
@@ -11,14 +11,11 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import argparse
-import os
 import sys
 from distutils.version import LooseVersion
-from signal import signal, SIGPIPE, SIG_DFL
 
-sys.path.insert(1, os.path.join(os.path.dirname(__file__), '..'))
-from _tool import ToolManager
-from utils import get_mem_usage
+from mess._tool import ToolManager
+from mess.utils import get_mem_usage
 
 DEBUG_MEM = 0  # set to 1 to print memory info
 
@@ -31,27 +28,32 @@ def check_dependencies():
         import pybel
         version = pybel.ob.OBReleaseVersion()
         if LooseVersion(version) < LooseVersion('2.3.0'):
-            sys.exit(emsg + ' Current version is %s.' % version)
-    except AttributeError, ImportError:
+            sys.exit('%s Current version is %s.' % (emsg, version))
+    except (AttributeError, ImportError):
         sys.exit(emsg)
 
 
 def main():
-    """Parse args and load the user-specified tool and execute it."""
+    """Parse args, load the user-specified tool and execute it."""
+    check_dependencies()
     toolmanager = ToolManager()
     parser = argparse.ArgumentParser(
         description='A collection of tools for interacting with MESS.DB',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     toolmanager.populate_parser(parser)
     args = parser.parse_args()
-    check_dependencies()
     tool = toolmanager.load_tool(args.subparser_name)
     tool.execute(args)
 
 if __name__ == '__main__':
-    signal(SIGPIPE, SIG_DFL)  # ignore SIG_PIPE, don't throw exception
-    if DEBUG_MEM:
-        print(get_mem_usage('Memory Start'), file=sys.stderr)
-    main()
-    if DEBUG_MEM:
-        print(get_mem_usage('Memeory Finish'), file=sys.stderr)
+    try:
+        if DEBUG_MEM:
+            print(get_mem_usage('Memory Start'), file=sys.stderr)
+        main()
+        if DEBUG_MEM:
+            print(get_mem_usage('Memeory Finish'), file=sys.stderr)
+    except IOError, err:
+        if err.errno == 32:
+            pass  # broken pipe, fail silently
+        else:
+            raise
