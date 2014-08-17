f0d1ec90ffd9097a8832b5e23996968e4fcfee17
Victor Amin <victor.amin@gmail.com>
2014-08-17 11:40:14 -0500

    fix connection keyword overriding

--- mess/_db.py
+++ mess/_db.py
@@ -26,18 +26,16 @@ class MessDB(object):
         max_tries (int): Maximum number of times to try db commit
         conn (obj): sqlite3 db connection object
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """Initialize attributes and db connection.
         
         Args:
-            All args will be passed to sqlite3.connect()
+            All named args will be passed to sqlite3.connect()
         """
-        self.args = args
-        self.kwargs = kwargs
         self.tries = 0
         self.max_tries = 3
         self.check_version()
-        self.open()
+        self.open(**kwargs)
     
     def check_version(self):
         """Ensure that all dependencies are satisfied."""
@@ -45,12 +43,15 @@ class MessDB(object):
             sys.exit(('The database requres SQLite version 3.7 or greater '
                       'due to the use of WAL mode.'))
     
-    def open(self):
+    def open(self, **kwargs):
         """Open a connection to db/mess.db and set up row factory."""
         try:
-            self.conn = sqlite3.connect(os.path.join(os.path.dirname(__file__),
-                                                     '../db/mess.db'),
-                                        timeout=120, *self.args, **self.kwargs)
+            if 'database' not in kwargs:
+                kwargs['database'] = os.path.join(os.path.dirname(__file__),
+                                                  '../db/mess.db')
+            if 'timeout' not in kwargs:
+                kwargs['timeout'] = 120
+            self.conn = sqlite3.connect(**kwargs)
         except IOError:
             sys.exit('could not find/create mess.db')
         self.conn.row_factory = self.namedtuple_factory
@@ -59,13 +60,13 @@ class MessDB(object):
     
     def reopen(self):
         """Commit, close, and open db connection."""
-        self.commit()
         self.close()
         self.open()
     
     def close(self):
         """Close the db connection."""
-        return self.conn.close()
+        self.commit()
+        self.conn.close()
     
     def cursor(self):
         """Return cursor."""
@@ -83,6 +84,7 @@ class MessDB(object):
         sys.exit('Database is still locked after %d tries.' % self.tries)
     
     def execute(self, query, values=()):
+        """Execute a single query."""
         try:
             with self.conn:
                 return self.conn.execute(query, values)
@@ -92,6 +94,7 @@ class MessDB(object):
                    '%s') % (query, values, err), file=sys.stderr)
             
     def executemany(self, query, values):
+        """Execute a single query with many values."""
         try:
             with self.conn:
                 return self.conn.executemany(query, values)
@@ -101,6 +104,7 @@ class MessDB(object):
                    '%s') % (query, err), file=sys.stderr)
     
     def executescript(self, script):
+        """Read and execute queries from file."""
         try:
             with self.conn:
                 return self.conn.executescript(script)
@@ -127,17 +131,16 @@ class MessDB(object):
     
     def initialize(self):
         """Load the mess.db schema."""
-        cur = self.cursor()
         tables = os.path.join(os.path.dirname(__file__),
                               '../db/schema/tables.sql')
         views = os.path.join(os.path.dirname(__file__),
                              '../db/schema/views.sql')
         triggers = os.path.join(os.path.dirname(__file__),
                                 '../db/schema/triggers.sql')
-        cur.executescript(codecs.open(tables, encoding='utf-8').read())
-        cur.executescript(codecs.open(views, encoding='utf-8').read())
-        cur.executescript(codecs.open(triggers, encoding='utf-8').read())
-        result = cur.execute('PRAGMA journal_mode=wal').next()
+        self.executescript(codecs.open(tables, encoding='utf-8').read())
+        self.executescript(codecs.open(views, encoding='utf-8').read())
+        self.executescript(codecs.open(triggers, encoding='utf-8').read())
+        result = self.execute('PRAGMA journal_mode=wal').next()
         if not result.journal_mode == 'wal':
             sys.exit(('Setting journal mode to WAL failed. Run PRAGMA '
                       'journal_mode=wal in SQLite before continuing.'))
@@ -146,7 +149,6 @@ class MessDB(object):
     def __del__(self):
         """On deletion, commit transations and close the connection."""
         try:
-            self.commit()
             self.close()
         except AttributeError:
             pass  # there was no db connection
