46dce0f588f3eb988c0f654912b7ab68e11ddd44
Victor Amin <victor.amin@gmail.com>
2014-08-31 11:56:35 -0500

    queries handled by mess select now

--- queries/mw.sql
+++
@@ -1,5 +0,0 @@
-SELECT m.inchikey, m.smiles, mmp.result as mw
-FROM molecule m
-JOIN molecule_method_property mmp ON mmp.inchikey=m.inchikey
-JOIN property p ON p.property_id=mmp.property_id
-WHERE p.name='MW' AND mmp.result BETWEEN 0 AND 10000;
