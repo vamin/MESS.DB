73de39034fc0fe77b00a7125580f554ad382876b
Victor Amin <victor.amin@gmail.com>
2014-10-06 00:18:41 -0500

    override default logging with custom logging

--- mess/mapreduce.py
+++ mess/mapreduce.py
@@ -1,4 +1,12 @@
-import logging
+# -*- coding: utf-8 -*-
+# Copyright 2013-2014 Victor Amin, http://vamin.net/
+
+"""MESS.DB mapreduce module
+
+This module implements the mapreduce paradigm by extending classes from the
+mincemeatpy module.
+"""
+
 import os
 import socket
 import sys
@@ -8,17 +16,28 @@ from collections import OrderedDict
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'mincemeatpy'))
 import mincemeat
 
+from mess.log import Log
+
+# override defualt logging use with custom Log class
+logging = Log('console')
+mincemeat.logging = Log('console')
+
 DEFAULT_PORT = mincemeat.DEFAULT_PORT
 
 
 class Client(mincemeat.Client, object):
+    """Extends mincemeat.Client to support 'declined' command"""
+    
     def __init__(self):
+        """Call parent init."""
         super(Client, self).__init__()
     
     def handle_connect(self):
+        """Keep handle_connect() pass."""
         pass
     
     def call_mapfn(self, command, data):
+        """Call mapfn, or decline if mapfn not set."""
         if not self.mapfn:
             time.sleep(1)
             self.send_command('declined')
@@ -35,19 +54,18 @@ class Client(mincemeat.Client, object):
             self.send_command('mapdone', (data[0], results))
     
     def call_reducefn(self, command, data):
+        """Call reducefn, or decline and close if reducefn not set."""
         if not self.reducefn:
             time.sleep(1)
             self.send_command('declined')
+            self.handle_close()
         else:
             logging.info("Reducing %s" % str(data[0]))
             results = self.reducefn(data[0], data[1])
             self.send_command('reducedone', (data[0], results))
     
-    def run(self, hostname, port, sleep=2, debug=0):
-        if debug:
-            logging.basicConfig(level=logging.DEBUG)
-        else:
-            logging.basicConfig(level=logging.INFO)
+    def run(self, hostname, port, sleep=2):
+        """Run client."""
         while True:
             try:
                 self.conn(hostname, port)
@@ -70,34 +88,41 @@ class Client(mincemeat.Client, object):
 
 
 class Server(mincemeat.Server):
+    """Extends mincemeat.Server to add 'run' method and to use extended
+    ServerChannel class"""
+    
     def __init__(self):
+        """Call parent init."""
         super(Server, self).__init__()
     
     def run(self, password=None, port=DEFAULT_PORT, debug=0):
+        """Run server."""
         if password is None:
             password = self.password
-        if debug:
-            logging.basicConfig(level=logging.DEBUG)
-        else:
-            logging.basicConfig(level=logging.INFO)
         return self.run_server(password, port)
     
     def handle_accept(self):
+        """Accept a connection, uses extended ServerChannel."""
         conn, addr = self.accept()
         sc = ServerChannel(conn, self.socket_map, self)
         sc.password = self.password
 
 
 class ServerChannel(mincemeat.ServerChannel):
+    """Extends mincemeat.ServerChannel to to support 'declined' command"""
+    
     def __init__(self, conn, map, server):
+        """Reimplement parent init."""
         mincemeat.Protocol.__init__(self, conn, map=map)
         self.server = server
         self.start_auth()
     
     def declined(self, command, data):
+        """Go to next task if a task is declined."""
         self.start_new_task()
     
     def process_command(self, command, data=None):
+        """Process 'mapdone', 'reducedone', and 'declined' commands."""
         commands = {
             'mapdone': self.map_done,
             'reducedone': self.reduce_done,
